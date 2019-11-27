4d577eea427c5a9856d396073fb1cd6aaecbbe7e
root <root@rtc-server-smu.us-central1-f.c.light-ether-251817.internal>
2019-11-27 18:29:52 +0000

    Modificar os arquivos para versão pessoal

--- demos/js/demo_audio_video.js
+++ demos/js/demo_audio_video.js
@@ -1,28 +1,3 @@
-//
-//Copyright (c) 2016, Skedans Systems, Inc.
-//All rights reserved.
-//
-//Redistribution and use in source and binary forms, with or without
-//modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright notice,
-//      this list of conditions and the following disclaimer.
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-//LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//POSSIBILITY OF SUCH DAMAGE.
-//
 var selfEasyrtcid = "";
 var haveSelfVideo = false;
 
@@ -36,7 +11,6 @@ function enable(domId) {
 }
 
 var onceOnly = true;
-
 
 function connect() {	
   easyrtc.enableAudio(document.getElementById("shareAudio").checked);
@@ -69,9 +43,6 @@ function hangup() {
     easyrtc.hangupAll();
     disable('hangupButton');
 }
-
-
-
 
 
 function clearConnectList() {
@@ -138,7 +109,7 @@ function loginSuccess(easyrtcid) {
     enable('otherClients');
     selfEasyrtcid = easyrtcid;
     document.getElementById("iam").innerHTML = "I am " + easyrtc.cleanId(easyrtcid);
-    easyrtc.showError("noerror", "logged in");
+ // easyrtc.showError("noerror", "logged in");
 }
 
 
